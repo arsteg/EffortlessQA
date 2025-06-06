f48b301ea6d75f0104ac40b6783f3b363774c62e
Mohd Rafi <dotnetexpertdev@gmail.com>
2025-06-06 21:01:40 +0530

    fixed the modal close and the drop down issues

--- EffortlessQA.Client/Program.cs
+++ EffortlessQA.Client/Program.cs
@@ -13,13 +13,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices(config =>
 {
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
     config.SnackbarConfiguration.PreventDuplicates = false;
-    config.SnackbarConfiguration.NewestOnTop = true;
+    config.SnackbarConfiguration.NewestOnTop = false;
     config.SnackbarConfiguration.ShowCloseIcon = true;
     config.SnackbarConfiguration.VisibleStateDuration = 5000;
     config.SnackbarConfiguration.HideTransitionDuration = 500;
     config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
 });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddTransient<AuthTokenHandler>();
