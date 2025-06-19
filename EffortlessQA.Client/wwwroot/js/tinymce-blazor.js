f5dd6dc4393dd270b98c1ef3d6a503ad3e327e51
Mohd Rafi <dotnetexpertdev@gmail.com>
2025-06-19 06:36:12 +0530

    fixed the rich text issue

---
+++ EffortlessQA.Client/wwwroot/js/tinymce-blazor.js
@@ -0,0 +1,21 @@
+﻿window.tinyMCEBlazor = {
+    initEditor: function (textareaId, dotNetHelper) {
+        tinymce.init({
+            selector: `#${textareaId}`,
+            height: 400,
+            plugins: 'image code link lists table',
+            toolbar: 'undo redo | formatselect | bold italic underline | alignleft aligncenter alignright | bullist numlist outdent indent | link image | code',
+            setup: function (editor) {
+                editor.on('change', function () {
+                    dotNetHelper.invokeMethodAsync('OnEditorChange', editor.getContent());
+                });
+            }
+        });
+    },
+    setContent: function (textareaId, content) {
+        tinymce.get(textareaId)?.setContent(content);
+    },
+    getContent: function (textareaId) {
+        return tinymce.get(textareaId)?.getContent();
+    }
+};
